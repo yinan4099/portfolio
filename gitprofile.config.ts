bead36a048da8e0ab94f630ac6098164c9fd437d
yinan4099 <136575472+yinan4099@users.noreply.github.com>
2024-03-16 01:10:57 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -173,7 +173,7 @@ const CONFIG = {
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
